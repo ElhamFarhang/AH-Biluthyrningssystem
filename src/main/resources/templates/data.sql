cbdb7e8ccb5e8e48023aa303e40e11d94ebbaa7e
ElhamFarhang <62466802+ElhamFarhang@users.noreply.github.com>
2025-04-22 15:15:31 +0200

    Delete src/main/resources/templates/data.sql

--- src/main/resources/templates/data.sql
+++
@@ -1,8 +0,0 @@
-
-
-INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Anna', 'Svensson', '19850101-1234', Skåne, Anna@mail.com, 0728686636, NULL);
-INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Erik', 'Johansson', '19900215-5678', Sundsvall, Erik@mail.com, 0733654289, NULL);
-INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Maria', 'Lindberg', '19751230-9101', Gävle, Maria@mail.com, NULL, NULL);
-INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Johan', 'Karlsson', '19881122-3456', Malmå, Johan@mail.com, Null, NULL );
-INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Elin', 'Andersson', '19950505-7890', Stockholm, Elin@mail.com, 0832565850, NULL);
-

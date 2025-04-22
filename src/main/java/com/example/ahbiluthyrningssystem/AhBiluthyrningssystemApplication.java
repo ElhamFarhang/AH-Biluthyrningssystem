6767dc162971f47b6cbb454370ce869d1dfef827
elham <elham1362@yahoo.com>
2025-04-22 10:47:11 +0200

    init

---
+++ src/main/java/com/example/ahbiluthyrningssystem/AhBiluthyrningssystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ahbiluthyrningssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhBiluthyrningssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhBiluthyrningssystemApplication.class, args);
+    }
+
+}

6b72e1604f5a4e8b548f58e591ad600da6e6f95b
theowm <theo.winter.milatovic@gmail.com>
2025-04-23 10:30:09 +0200

    Lagt till Car-entiteten

---
+++ src/main/java/com/example/ahbiluthyrningssystem/entities/Car.java
@@ -0,0 +1,91 @@
+package com.example.ahbiluthyrningssystem.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 15, nullable = false)
+    private int pricePerDay;
+
+    @Column(length = 30, nullable = false)
+    private String make;
+
+    @Column(length = 30, nullable = false)
+    private String model;
+
+    @Column(length = 20, nullable = false)
+    private String registrationNumber;
+
+    @Column(length = 10, nullable = false)
+    private boolean isBooked;
+
+    public Car() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(int pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", pricePerDay=" + pricePerDay +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", isBooked=" + isBooked +
+                '}';
+    }
+}

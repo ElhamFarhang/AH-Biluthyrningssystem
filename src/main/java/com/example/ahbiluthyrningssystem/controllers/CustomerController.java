3de8c9fccfcf2cfa0b7fdd9cca988c4879f6b5fa
wiha97 <wiha97@duck.com>
2025-04-23 11:07:27 +0200

    Updated credits

--- src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
+++ src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
@@ -11,31 +11,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class CustomerController {
+    //  Wille
     @GetMapping("/cars")
     public ResponseEntity<List<Car>> getAvailableCars() {
         return null;
     }
 
+    //  Wille
     @PostMapping("/addorder")
     public ResponseEntity<Order> addOrder(@RequestBody Order order) {
         return null;
     }
 
+    //  Wille
     @PutMapping("/cancelorder")
     public ResponseEntity<Order> cancelOrder(@RequestBody Order order) {
         return null;
     }
 
+    //  Wille
     @GetMapping("/activeorders")
     public ResponseEntity<List<Order>> getActiveOrders() {
         return null;
     }
 
+    //  Wille
     @GetMapping("/orders")
     public ResponseEntity<List<Order>> getOrders() {
         return null;
     }
 
+    //  Wille
     @PutMapping("/updateinfo")
     public ResponseEntity<Customer> updateOrder(@RequestBody Customer customer) {
         return null;

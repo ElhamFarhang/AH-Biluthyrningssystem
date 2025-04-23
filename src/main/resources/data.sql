b825e83c42f2e7fc63113ffc35c510dee5488f47
ElhamFarhang <62466802+ElhamFarhang@users.noreply.github.com>
2025-04-23 11:01:19 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
-
+//--------------------- Elham - customersData --------------
 INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Anna', 'Svensson', '19850101-1234', 'Skåne', 'Anna@mail.com', '0728686636', null);
 INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Erik', 'Johansson', '19900215-5678', 'Sundsvall', 'Erik@mail.com', '0733654289', null);
 INSERT INTO CUSTOMER (first_name, last_name, personal_number, address, email, phone_number, order_id) VALUES ('Maria', 'Lindberg', '19751230-9101', 'Gävle', 'Maria@mail.com', null, null);

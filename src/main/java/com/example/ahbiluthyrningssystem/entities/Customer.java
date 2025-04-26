571aecfd0e4431e72265531928783b2946ce8ce1
ElhamFarhang <62466802+ElhamFarhang@users.noreply.github.com>
2025-04-26 15:17:51 +0200

    Update Customer.java

--- src/main/java/com/example/ahbiluthyrningssystem/entities/Customer.java
+++ src/main/java/com/example/ahbiluthyrningssystem/entities/Customer.java
@@ -10,7 +10,7 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "Customer-id")
+    @Column (name = "Customer_id")
     private Integer id;
     @Column (length = 30, nullable = false)
     private String first_name;

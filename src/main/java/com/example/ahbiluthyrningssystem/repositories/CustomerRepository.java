f14bd7100f99373e195ad94a1602db5f3bedf184
elham <elham1362@yahoo.com>
2025-04-26 14:58:07 +0200

    customer repositoriy &  Service

---
+++ src/main/java/com/example/ahbiluthyrningssystem/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.ahbiluthyrningssystem.repositories;
+
+import com.example.ahbiluthyrningssystem.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}

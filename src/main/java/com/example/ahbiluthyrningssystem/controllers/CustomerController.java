246fb431ac345ab1fb1321bc642b7667df98a910
wiha97 <wiha97@duck.com>
2025-04-27 10:28:13 +0200

    fixed merge

--- src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
+++ src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
@@ -1,49 +1,62 @@
-package com.example.ahbiluthyrningssystem.controllers;
+package com.example.Enterprise.Java.Individuell.laboration.ne.Exceptions;
 
 import com.example.ahbiluthyrningssystem.entities.Car;
 import com.example.ahbiluthyrningssystem.entities.Customer;
 import com.example.ahbiluthyrningssystem.entities.Order;
+import com.example.ahbiluthyrningssystem.services.CustomerServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//--------------------- Wille & Elham - NotAcceptableException --------------
 @RestController
 @RequestMapping("/api/v1")
 public class CustomerController {
-    //  Wille
+
+    private CustomerServiceImp customerServiceImp;
+
+    // Elham
+    @Autowired
+    public CustomerController(CustomerServiceImp customerServiceImp) {
+        this.customerServiceImp = customerServiceImp;
+    }
+
+    //  Wille & Elham
     @GetMapping({"/cars", "/admin/cars"})
     public ResponseEntity<List<Car>> getAvailableCars() {
-        return null;
+        return ResponseEntity.ok(customerServiceImp.getAllCars());
     }
 
-    //  Wille
+    //  Wille & Elham
     @PostMapping("/addorder")
     public ResponseEntity<Order> addOrder(@RequestBody Order order) {
-        return null;
+        return ResponseEntity.ok(customerServiceImp.addOrder(order));
     }
 
-    //  Wille
-    @PutMapping("/cancelorder")
-    public ResponseEntity<Order> cancelOrder(@RequestBody Order order) {
-        return null;
+    //  Wille & Elham
+    @PutMapping("/cancelorder/{id}")
+    public ResponseEntity<String> cancelOrder(@PathVariable("id") Integer id) {
+        customerServiceImp.cancelOrder(id);
+        return ResponseEntity.ok("Order with ID \" + id + \" has been successfully cancelled.");
     }
 
-    //  Wille
+    //  Wille & Elham
     @GetMapping("/activeorders")
     public ResponseEntity<List<Order>> getActiveOrders() {
-        return null;
+        return ResponseEntity.ok(customerServiceImp.getActiveOrders());
     }
 
-    //  Wille
+    //  Wille & Elham
     @GetMapping("/orders")
     public ResponseEntity<List<Order>> getOrders() {
-        return null;
+        return ResponseEntity.ok(customerServiceImp.getAllOrders());
     }
 
-    //  Wille
-    @PutMapping("/updateinfo")
-    public ResponseEntity<Customer> updateOrder(@RequestBody Customer customer) {
-        return null;
+    //  Wille & Elham
+    @PutMapping("/updateinfo/{id}")
+    public ResponseEntity<Customer> updateInfo(@PathVariable("id") Integer id, @RequestBody Customer customer) {
+        return ResponseEntity.ok(customerServiceImp.updateInfo(id, customer));
     }
 }

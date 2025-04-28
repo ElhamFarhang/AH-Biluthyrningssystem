9a0326081ab1e44c3c8e98eaa9f3e8bbe8474079
AnnaB <altea200@hotmail.com>
2025-04-28 15:18:40 +0200

    JsonIgnoreProperties

--- src/main/java/com/example/ahbiluthyrningssystem/repositories/OrderRepository.java
+++ src/main/java/com/example/ahbiluthyrningssystem/repositories/OrderRepository.java
@@ -5,9 +5,23 @@ import com.example.ahbiluthyrningssystem.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {   //Anna
 
+    List<Order> findByCustomerIdAndActiveTrue(Integer customerId);
+    List<Order> findByCustomerIdAndActiveFalse(Integer customerId);
+    List<Order> findByCustomerIdAndActiveFalse(Integer customerId);
+    List<Order> findByActiveTrue();
+    List<Order> findByActiveFalse();
+
+
+
+/*     /api/v1/admin/activeorders- Lista alla aktiva ordrar
+ • GET /api/v1/admin/orders - Lista historiska ordrar
+ • DELETE /api/v1/admin/removeorder- Ta bort bokning från systemet
+ • DELETE /api/v1/admin/removeorders-beforedate/{date*/
 
 
 }

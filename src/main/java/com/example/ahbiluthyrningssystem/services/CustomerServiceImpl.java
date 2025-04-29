16868df0d211c15e302d691f2ea38596b99d7e33
elham <elham1362@yahoo.com>
2025-04-29 11:15:31 +0200

    loggar-costumerServiceImpl

--- src/main/java/com/example/ahbiluthyrningssystem/services/CustomerServiceImpl.java
+++ src/main/java/com/example/ahbiluthyrningssystem/services/CustomerServiceImpl.java
@@ -8,8 +8,6 @@ import com.example.ahbiluthyrningssystem.repositories.CustomerRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;

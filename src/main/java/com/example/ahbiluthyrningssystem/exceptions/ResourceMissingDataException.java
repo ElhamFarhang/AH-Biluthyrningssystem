52d402319aecb37c54fa0d90d85e1a4b0f26bbdf
wiha97 <wiha97@duck.com>
2025-05-01 20:59:35 +0200

    Added MissingDataException, updated .gitignore

---
+++ src/main/java/com/example/ahbiluthyrningssystem/exceptions/ResourceMissingDataException.java
@@ -0,0 +1,11 @@
+package com.example.ahbiluthyrningssystem.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class ResourceMissingDataException extends RuntimeException {
+    public ResourceMissingDataException(String object, String field) {
+        super(String.format("Object '%s' has no field '%s'", object, field));
+    }
+}

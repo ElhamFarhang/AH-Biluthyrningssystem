8b3e4ec2e3deb773ca078ffc4bca49e65a8a3d81
wiha97 <wiha97@duck.com>
2025-05-01 14:33:50 +0200

    Updated getmapping for both customers and admin

--- src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
+++ src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
@@ -31,7 +31,7 @@ public class CustomerController {
     }
 
     //  Wille & Elham
-    @GetMapping("/cars")
+    @GetMapping({"/cars", "/admin/cars"})
     public ResponseEntity<List<Car>> getAvailableCars(){
         return ResponseEntity.ok(carServiceImpl.getAvailableCars());
     }

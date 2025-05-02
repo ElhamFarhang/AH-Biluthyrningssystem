557e9dac334466a0d9fe298e52a78e61c311e04d
wiha97 <wiha97@duck.com>
2025-05-02 13:21:29 +0200

    Removed temp method for principal check

--- src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
+++ src/main/java/com/example/ahbiluthyrningssystem/controllers/CustomerController.java
@@ -60,7 +60,6 @@ public class CustomerController {
     @GetMapping("/orders")
     public ResponseEntity<List<Order>> getOrders(Principal principal) {
         orderServiceImpl.setPrincipal(principal);
-        System.out.println("Name: "+principal.getName());
         return ResponseEntity.ok(orderServiceImpl.getAllOrders());
     }
 
@@ -70,9 +69,5 @@ public class CustomerController {
         return ResponseEntity.ok(customerServiceImpl.updateInfo(id, customer));
     }
 
-    //Elham
-    @GetMapping("/authenticated")
-    public String authenticated(Principal principal) {
-        return "You are logged as: " + principal.getName();
-    }
+
 }

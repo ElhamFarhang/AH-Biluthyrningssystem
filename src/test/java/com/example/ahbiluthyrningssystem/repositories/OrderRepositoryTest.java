33947f95febefc1203bffdc3198a0e1d6f852eaf
AnnaB <altea200@hotmail.com>
2025-05-05 10:48:21 +0200

    OrderService väntar bara på Cars

---
+++ src/test/java/com/example/ahbiluthyrningssystem/repositories/OrderRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.example.ahbiluthyrningssystem.repositories;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findByCustomerPersonalnumberAndCanceledFalseAndDateEndAfter() {
+    }
+
+    @Test
+    void findByCustomerPersonalnumberAndCanceledTrueOrDateEndBefore() {
+    }
+
+    @Test
+    void findByCanceledFalseAndDateEndAfter() {
+    }
+
+    @Test
+    void findByCanceledTrueOrDateEndBefore() {
+    }
+
+    @Test
+    void deleteByDateEndBefore() {
+    }
+}
---
+++ src/test/java/com/example/ahbiluthyrningssystem/services/OrderServiceImplTest.java
@@ -0,0 +1,54 @@
+package com.example.ahbiluthyrningssystem.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void setPrincipal() {
+    }
+
+    @Test
+    void addOrder() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void getActiveOrdersCustomer() {
+    }
+
+    @Test
+    void getOldOrdersCustomer() {
+    }
+
+    @Test
+    void getActiveOrdersAdmin() {
+    }
+
+    @Test
+    void getOldOrdersAdmin() {
+    }
+
+    @Test
+    void deleteOrder() {
+    }
+
+    @Test
+    void deleteAllOrdersBeforeDate() {
+    }
+}

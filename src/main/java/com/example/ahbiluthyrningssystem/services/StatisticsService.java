f04bb4588bafbf846050d0259da56157493e6aef
AnnaB <altea200@hotmail.com>
2025-05-06 14:13:59 +0200

    spar

---
+++ src/main/java/com/example/ahbiluthyrningssystem/services/StatisticsService.java
@@ -0,0 +1,76 @@
+package com.example.ahbiluthyrningssystem.services;
+
+import com.example.ahbiluthyrningssystem.entities.Car;
+import com.example.ahbiluthyrningssystem.entities.Order;
+import com.example.ahbiluthyrningssystem.repositories.CarRepository;
+import com.example.ahbiluthyrningssystem.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class StatisticsService {
+
+    private final OrderRepository orderRepo;
+    private final CarRepository carRepo;
+
+    @Autowired
+    public StatisticsService(OrderRepository orderRepo, CarRepository carRepo){
+        this.carRepo = carRepo;
+        this.orderRepo = orderRepo;
+    }
+
+    public Map<String, Integer> mostRentedMake(LocalDate start, LocalDate End){
+        Map<String, Integer> mostRented = new HashMap<>();
+        List<Order> orders = orderRepo.findAll();
+        String make = "";
+        int times = 0;
+        Map<String, List<Order>> group = orders.stream().collect(Collectors.groupingBy(o -> o.getCar().getMake()));
+        mostRented.put(make, times);
+        return mostRented;
+    }
+
+/*
+    public List<Map<String, Integer>> timesCarRented() {
+        List<Map<String, Integer>>
+        timesRented.put("registrationNumber", null);
+        return timesRented;
+    }
+*/
+
+    public int mostCommonPeriod() {
+        int days = 0;
+        return days;
+    }
+
+
+
+    public int averageOrderCost() {
+        int cost = 0;
+        return cost;
+    }
+
+
+
+
+    public int totalIncomeCar() {
+        int cost = 0;
+        return cost;
+    }
+
+
+
+
+    public int totalIncomePeriod(LocalDate start, LocalDate End) {
+        int cost = 0;
+        return cost;
+    }
+
+
+
+}
